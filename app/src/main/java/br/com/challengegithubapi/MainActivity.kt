e3d0a9986243562dc5871117c1351e1acf92e40f
Michel Kim <kim98.michel@gmail.com>
2023-05-17 23:06:35 -0300

    set navigation and Home fragment to nav_graph

--- app/src/main/java/br/com/challengegithubapi/MainActivity.kt
+++ app/src/main/java/br/com/challengegithubapi/MainActivity.kt
@@ -2,10 +2,25 @@ package br.com.challengegithubapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import br.com.challengegithubapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        setNavController()
+    }
+
+    private fun setNavController() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
